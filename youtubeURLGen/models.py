974e6da7ac8d142e837a8f00f57427ab279c33e8
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-07 12:25:30 -0600

    "shows values of urls now"

--- youtubeURLGen/models.py
+++ youtubeURLGen/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class urlInput(models.Model):
-    web_url = models.URLField(range(33-43), unique = True)
+    web_url = models.URLField(unique = True)
 
     def __unicode__(self):
         return self.web_url
