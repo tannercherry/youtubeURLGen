4ff34b0c838f601461350a7a7c4a21cb75b23195
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-08 23:42:37 -0600

    "testing to parse url"

--- youtubeURLGen/models.py
+++ youtubeURLGen/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
+
+'''optional function to try'''
+import re
+
+def getNormalized(url):
+    return re.sub('https://www.youtube.com/watch?v=', url)
+
 class urlInput(models.Model):
-    web_url = models.URLField(unique = True)
+    web_url = models.URLField(unique = True
 
     def __unicode__(self):
         return self.web_url.replace('https://www.youtube.com/watch?v=', '')
--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -3,19 +3,11 @@ from youtubegen.forms import urlForm
 
 from .models import urlInput
 
-import re
-
-def getNormalized(url):
-    return re.sub(r'https://youtube.com/watch?v=', r'', url)
-
 def input(request):
     form = urlForm()
-
     if request.POST:
         form = urlForm(request.POST)
         if form.is_valid():
-          '''form = str(form)'''
-          '''getNormalized(form)'''
           form.save()
 
     return render(request, 'urlInput/index.html', {'form': form})
