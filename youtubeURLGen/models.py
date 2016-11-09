5bbf0bb6a69abfcfc71b2fbd948a70181b702919
tanner.cherry <tanner.cherry@gmail.com>
2016-11-09 14:13:23 -0600

    "testing to parse url"

--- youtubeURLGen/models.py
+++ youtubeURLGen/models.py
@@ -1,19 +1,7 @@
 from django.db import models
 
-
-
-'''optional function to try'''
-import re
-
-def getNormalized(url):
-    url = url.replace('https://www.youtube.com/watch?v=', '')
-    return url
-
-'''end function'''
-
 class urlInput(models.Model):
     web_url = models.URLField(max_length = 43, unique = True)
-    getNormalized(web_url)
 
     def __unicode__(self):
         return self.web_url
--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from youtubegen.forms import urlForm
-
 from .models import urlInput
 
 def input(request):
@@ -14,8 +13,5 @@ def input(request):
 
 def urls(request):
     urlAddresses = urlInput.objects.all().values_list()
-
-    url_list = list(urlAddresses)
-    '''try above function for changing queryset to list'''
 
     return render(request, 'urlInput/urls.html', {'urlAddresses' : urlAddresses})
