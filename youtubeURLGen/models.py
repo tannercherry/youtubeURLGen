6155dcd45b34ee39243a2b204eee3692b38632e8
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-10 00:43:10 -0600

    "testing to parse url"

--- youtubeURLGen/models.py
+++ youtubeURLGen/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class urlInput(models.Model):
-    web_url = models.URLField(unique = True)
+    web_url = models.CharField(max_length = 43, unique = True)
 
     def __str__(self):
         return self.web_url
--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -2,12 +2,6 @@ from django.shortcuts import render
 from youtubegen.forms import urlForm
 from .models import urlInput
 
-from urllib.parse import urlparse
-
-def url(value):
-    parsedURL = urlparse(value)
-    return parsedURL.argument
-
 def input(request):
     form = urlForm()
     if request.POST:
@@ -19,14 +13,5 @@ def input(request):
 
 def urls(request):
     urlAddresses = urlInput.objects.all().values_list()
-    url_list = list(urlAddresses)
-    a = 0
-    num = len(url_list) - 1
-    while a >= num:
-        if type(url_list[a]) == int:
-            a = a + 1
-        else:
-            url(url_list[a])
-            a = a + 1
 
-    return render(request, 'urlInput/urls.html', {'url_list' : url_list})
+    return render(request, 'urlInput/urls.html', {'url_list' : urlAddresses})
