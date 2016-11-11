8d4af9d463a7a017a7a41425d5d94142e8b1cedc
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-11 10:59:55 -0600

    fixed views, working url parse

--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -8,8 +8,8 @@ def input(request):
         extra_form = form.save(commit = False)
         web = extra_form.web_url
         web_string = web.replace("https://www.youtube.com/watch?v=", "")
-        form.web_url = web_string
-        form.save(commit = True)
+        new_input = urlInput(web_url=web_string)
+        new_input.save()
     return render(request, 'urlInput/index.html', {'form': form})
 
 def urls(request):
