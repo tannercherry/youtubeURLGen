d9486888efe6bcfc64cb3d83a1a86ab12862b929
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-11 00:42:01 -0600

    "changing form fields to parse url"

--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -2,20 +2,14 @@ from django.shortcuts import render
 from youtubegen.forms import urlForm
 from .models import urlInput
 
-import re
-
-def clean_web_url(self):
-    web = self.data['web_url']
-    web_string = re.sub("https://www.youtube.com/watch?v=", "", web)
-    return web_string
-
 def input(request):
     form = urlForm(request.POST)
     if request.method == "POST" and form.is_valid():
         form.save(id)
-        form.web_url = clean_web_url(form)
-        form.save('web_url')
-
+        web = form.data['web_url']
+        web_string = web[33:]
+        form.web_url = web_string
+        '''find way to save string as form here'''
     return render(request, 'urlInput/index.html', {'form': form})
 
 def urls(request):
