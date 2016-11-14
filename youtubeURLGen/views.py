1fa1df61ee0b79b836946f912b2edba8d8ca4194
[tannercherry] <[tanner.cherry@gmail.com]>
2016-11-14 12:50:50 -0600

    "can take different inputs"

--- youtubeURLGen/views.py
+++ youtubeURLGen/views.py
@@ -7,10 +7,33 @@ def input(request):
     if form.is_valid():
         extra_form = form.save(commit = False)
         web = extra_form.web_url
-        web_string = web.replace("https://www.youtube.com/watch?v=", "")
-        new_input = urlInput(web_url=web_string)
-        new_input.validate_unique()
-        new_input.save()
+        if web.startswith("https://www.youtube.com/watch?v="):
+            web_string = web.replace("https://www.youtube.com/watch?v=", "")
+            new_input = urlInput(web_url=web_string)
+            new_input.validate_unique()
+            new_input.save()
+        elif web.startswith("www.youtube.com/watch?v="):
+            web_string = web.replace("www.youtube.com/watch?v=", "")
+            new_input = urlInput(web_url=web_string)
+            new_input.validate_unique()
+            new_input.save()
+        elif web.startswith("http://www.youtube.com/watch?v="):
+            web_string = web.replace("http://www.youtube.com/watch?v=", "")
+            new_input = urlInput(web_url=web_string)
+            new_input.validate_unique()
+            new_input.save()
+        elif web.startswith("https://youtube.com/watch?v="):
+            web_string = web.replace("https://youtube.com/watch?v=", "")
+            new_input = urlInput(web_url=web_string)
+            new_input.validate_unique()
+            new_input.save()
+        elif web.startswith("youtube.com/watch?v="):
+            web_string = web.replace("youtube.com/watch?v=", "")
+            new_input = urlInput(web_url=web_string)
+            new_input.validate_unique()
+            new_input.save()
+        else:
+            print ("Error, please try a new url")
     return render(request, 'urlInput/index.html', {'form': form})
 
 def urls(request):
